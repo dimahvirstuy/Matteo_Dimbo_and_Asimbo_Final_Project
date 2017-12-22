4588d1bee0198e9b0448c993879c58e6ecbd7949
dimahvirstuy <dhvirtsman@stuy.edu>
2017-12-22 11:38:23 -0500

    a

---
+++ V1/heads.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <signal.h>
+#include <sys/stat.h>
